a167634e7ae6777c384616f92cf8fc3ef6056c38
Ludwig Simonsson <93864945+LudwigSimonsson@users.noreply.github.com>
2023-06-08 08:08:32 +0200

    Update Led.h

--- Led.h
+++ Led.h
@@ -3,6 +3,23 @@
 
 #include <stdint.h>
 #include "uart.h"
+#include "stm32f4xx.h"                  
+
+#define	LED_PORT 		GPIOB
+
+#define	LED_PORT_CLOCK  (1U<<1)
+
+#define LED_RED_PIN	(1U<<14)
+#define LED_GREEN_PIN	(1U<<12)
+#define	LED_BLUE_PIN	(1U<<15)
+#define LED_YELLOW_PIN	(1U<<13)
+
+#define LED_RED_MODE_BIT  (1U<<28)
+#define LED_GREEN_MODE_BIT (1U<<24)
+#define LED_YELLOW_MODE_BIT (1U<<26)
+#define LED_BLUE_MODE_BIT (1U<<30)
+
+
 
 typedef  enum {
   
@@ -18,6 +35,9 @@ typedef enum{
  OFF =0,
  ON = 1
 }LedState_Type;
+
+
+
 
 typedef struct{
 	
